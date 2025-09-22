c6cca4a627e98008aa3a7337b5f902542630b557
stmelnik2020 <stmelnik2020@gmail.com>
2025-09-22 18:37:08 +0200

    1

--- m16_01_02/Structural/Decorator/example02.py
+++ m16_01_02/Structural/Decorator/example02.py
@@ -60,10 +60,10 @@ def baz(name):
 
 
 if __name__ == '__main__':
-    # long_loop(1_000_000)
-    # print(long_loop.__name__)
-    # print(long_loop.__doc__)
-    # print(long_loop.__annotations__)
+    long_loop(1_000_00000)
+    print(long_loop.__name__)
+    print(long_loop.__doc__)
+    print(long_loop.__annotations__)
     print(baz("Edward"))
     print(baz.__wrapped__("Oleksandr"))
     print(baz.__wrapped__.__wrapped__("Evgeny"))
